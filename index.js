c78a54842e6ece8010a0f761b43810b1c9920c0a
paikwiki <paikwiki@gmail.com>
2018-02-13 01:13:30 +0900

    Change demicalNum to HexademicalNum:apple:
    - Add console.log to print device.info

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ var devices = HID.devices();
 console.log(devices.map(d=>({pname: d.product, vid: d.vendorId, pid: d.productId})));
 
 var deviceInfo = devices.find( function(d) {
-    var isUSBKeyStick = d.vendorId===3829 && d.productId===12288;
+    var isUSBKeyStick = d.vendorId===0xEF5 && d.productId===0x3000;
 
     return isUSBKeyStick;
 });
 
 if( deviceInfo ) {
+  console.log(deviceInfo);
   var device = new HID.HID( deviceInfo.path );
 
   // use device
